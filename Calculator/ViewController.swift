54d6a128caec6f84f79e11f3438384c7f0bb7f10
timchuchok <timchenko.vlad@gmail.com>
2017-07-06 12:55:22 +0300

    storyboard is done

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func clearResultLabel(_ sender: UIButton) {
+        resultLabel.text = "0"
+    }
+    
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        if resultLabel.text!.characters.count < 10 {
+            if resultLabel.text! == "0" {
+                resultLabel.text! = sender.currentTitle!
+            } else {
+                resultLabel.text! += sender.currentTitle!
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        resultLabel.text = "0"
     }
 
     override func didReceiveMemoryWarning() {
